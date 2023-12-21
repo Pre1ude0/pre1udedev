1644a52dd676bb9b4fa2ad222fcaaec07a3d9e54
LarssJakobsons <larss.jakobsons@icloud.com>
2023-12-21 23:14:09 +0200

    Add multithreading support to Flask app

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from threading import Thread
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return render_template('main.html')
+    return render_template("main.html")
 
 
-if __name__ == '__main__':
-    app.run()   
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+
+def start():
+    t = Thread(target=run)
+    t.start()
+
+start()
