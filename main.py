692c2635c812eb19943cdc2a4549bab2887faff1
LarssJakobsons <larss.jakobsons@icloud.com>
2023-12-21 23:16:30 +0200

    Revert "Add multithreading support to Flask app"
    This reverts commit 1644a52dd676bb9b4fa2ad222fcaaec07a3d9e54.

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from threading import Thread
 
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def hello():
-    return render_template("main.html")
+    return render_template('main.html')
 
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
-
-def start():
-    t = Thread(target=run)
-    t.start()
-
-start()
+if __name__ == '__main__':
+    app.run()   
