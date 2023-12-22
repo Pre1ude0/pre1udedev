db85790d40080111b0895b800693ee9c533ffeb5
LarssJakobsons <larss.jakobsons@icloud.com>
2023-12-22 23:27:14 +0200

    Update SSL route in app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/.well-known/acme-challenge/MdTW8IzYdFuKQWAekjWk1q2sP6sJAQhSczvPl1ovXZM")
+@app.route("/.well-known/acme-challenge/zXGv5YEGm92Lf9VFRA2Lvt7q8_BJ-oVOIUbxKZXtVrI"
+)
 def ssl():
     response = make_response(
         "MdTW8IzYdFuKQWAekjWk1q2sP6sJAQhSczvPl1ovXZM.xc9It2P3kCFnFCqMekQ-90CRf6tm3amb_WKVXc-QmMA",
