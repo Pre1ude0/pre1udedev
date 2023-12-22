ee2acce2a7fdd561b7f15b4b2ef88537917bad57
LarssJakobsons <larss.jakobsons@icloud.com>
2023-12-22 23:05:39 +0200

    Add SSL route and update CSS styles

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def main():
     return render_template("main.html")
+
+
+@app.route("/.well-known/acme-challenge/MdTW8IzYdFuKQWAekjWk1q2sP6sJAQhSczvPl1ovXZM")
+def ssl():
+    return "MdTW8IzYdFuKQWAekjWk1q2sP6sJAQhSczvPl1ovXZM.xc9It2P3kCFnFCqMekQ-90CRf6tm3amb_WKVXc-QmMA"
 
 
 if __name__ == "__main__":
