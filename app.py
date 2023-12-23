4f4adbae680fff5f3c25a1d3a5e43975d97ce687
LarssJakobsons <larss.jakobsons@icloud.com>
2023-12-23 22:17:37 +0200

    Paralax background addition and opengl contents

--- app.py
+++ app.py
@@ -15,17 +15,5 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
-
-@app.route("/.well-known/acme-challenge/zXGv5YEGm92Lf9VFRA2Lvt7q8_BJ-oVOIUbxKZXtVrI"
-)
-def ssl():
-    response = make_response(
-        "MdTW8IzYdFuKQWAekjWk1q2sP6sJAQhSczvPl1ovXZM.xc9It2P3kCFnFCqMekQ-90CRf6tm3amb_WKVXc-QmMA",
-        200,
-    )
-    response.mimetype = "text/plain"
-    return response
-
-
 if __name__ == "__main__":
     app.run()
