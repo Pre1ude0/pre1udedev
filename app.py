c5d915aaad9d37eea9163fa6121f68577f7e554d
LarssJakobsons <larss.jakobsons@icloud.com>
2023-12-26 17:20:43 +0200

    Add routes for projects and blogs and navbar

--- app.py
+++ app.py
@@ -15,5 +15,18 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/blogs")
+def blogs():
+    return render_template("blogs.html")
+
+@app.errorhandler(404)
+def unknown(e):
+    return render_template("unknown.html"), 404
+
+
 if __name__ == "__main__":
     app.run()
---
+++ static/scripts/blogs.js
@@ -0,0 +1,21 @@
+var dropdown_options = {
+  coverTrigger: false,
+  constrainWidth: false,
+  alignment: 'right',
+  closeOnClick: true,
+  hover: false,
+  inDuration: 100,
+  outDuration: 130,
+  click: true,
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdown_elems = document.querySelectorAll('.top-bar-trigger-box');
+    var dropdown_instances = M.Dropdown.init(dropdown_elems, dropdown_options);
+
+    if (window.innerWidth <= 768) {
+        // Change the CSS to fit a mobile device
+        document.body.classList.add('mobile-view');
+    }
+
+});
