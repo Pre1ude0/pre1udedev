916c8ea3128b28f55b950585d7aba97276845735
LarssJakobsons <larss.jakobsons@icloud.com>
2024-01-02 22:32:12 +0200

    Refactor dropdown menu to top bar navigation menu and improve code structure

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,13 +1,3 @@
-var dropdown_options = {
-  coverTrigger: false,
-  constrainWidth: false,
-  alignment: 'right',
-  closeOnClick: true,
-  hover: false,
-  inDuration: 100,
-  outDuration: 130,
-  click: true,
-};
 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.parallax');
