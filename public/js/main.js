398fd4256eefcce2b859fd1db6a5e6cb81f2bef6
Pre1ude0 <larss.jakobsons@icloud.com>
2025-01-18 23:58:12 +0200

    fix animations

--- public/js/main.js
+++ public/js/main.js
@@ -3,61 +3,68 @@ const indicator = document.getElementById("indicator");
 const copyPopup = document.getElementById("copyPopup");
 
 function changeScreen(screenId) {
-  for (i = 0; i < document.getElementsByClassName("screen").length; i++) {
-    if (document.getElementsByClassName("screen")[i].id !== screenId) {
-      document.getElementsByClassName("screen")[i].style.opacity = "0";
-      document.getElementsByClassName("screen")[i].style.zIndex = "0";
+    for (i = 0; i < document.getElementsByClassName("screen").length; i++) {
+        if (document.getElementsByClassName("screen")[i].id !== screenId) {
+            document.getElementsByClassName("screen")[i].style.opacity = "0";
+            document.getElementsByClassName("screen")[i].style.zIndex = "0";
+        }
+        document.getElementById(screenId).style.opacity = "1";
+        document.getElementById(screenId).style.zIndex = "5";
     }
-    document.getElementById(screenId).style.opacity = "1";
-    document.getElementById(screenId).style.zIndex = "5";
-  }
 }
 
 window.addEventListener("hashchange", function () {
-  if (window.location.hash === "#connections") {
-    indicator.style.bottom = "65px";
-    changeScreen("screen2");
-  } else if (window.location.hash === "#projects") {
-    indicator.style.bottom = "15px";
-    changeScreen("screen3");
-  } else if (window.location.hash === "#home") {
-    indicator.style.bottom = "115px";
-    changeScreen("screen1");
-  }
+    if (window.location.hash === "#connections") {
+        indicator.style.bottom = "65px";
+        changeScreen("screen2");
+    } else if (window.location.hash === "#projects") {
+        indicator.style.bottom = "15px";
+        changeScreen("screen3");
+    } else if (window.location.hash === "#home") {
+        indicator.style.bottom = "115px";
+        changeScreen("screen1");
+    }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (window.location.hash === "#connections") {
-    indicator.style.bottom = "65px";
-    changeScreen("screen2");
-  } else if (window.location.hash === "#projects") {
-    indicator.style.bottom = "15px";
-    changeScreen("screen3");
-  } else if (window.location.hash === "#home") {
-    document.getElementById("indicator").style.bottom = "115px";
-    changeScreen("screen1");
-  }
+    if (window.location.hash === "#connections") {
+        document
+            .getElementsByClassName("tab-selector")[0]
+            .classList.add("not-home");
+        indicator.style.bottom = "65px";
+        changeScreen("screen2");
+    } else if (window.location.hash === "#projects") {
+        document
+            .getElementsByClassName("tab-selector")[0]
+            .classList.add("not-home");
+
+        indicator.style.bottom = "15px";
+        changeScreen("screen3");
+    } else if (window.location.hash === "#home") {
+        document.getElementById("indicator").style.bottom = "115px";
+        changeScreen("screen1");
+    }
 });
 
 function copyItem(item) {
-	navigator.clipboard.writeText(item);
-	elementY = event.target.getBoundingClientRect().top;
-	elementX = event.target.getBoundingClientRect().left;
-	targetWidth = event.target.offsetWidth;
-	let leftOffset = elementX + targetWidth / 2;
-	leftOffset = leftOffset - copyPopup.offsetWidth / 2;
-	copyPopup.style.top = `calc(${elementY}px - 40px)`;
-	copyPopup.style.left = `${leftOffset}px`;
-	copyPopup.classList.add("visible");
-	setTimeout(() => {
-		copyPopup.classList.remove("visible");
-	}, 100);
+    navigator.clipboard.writeText(item);
+    elementY = event.target.getBoundingClientRect().top;
+    elementX = event.target.getBoundingClientRect().left;
+    targetWidth = event.target.offsetWidth;
+    let leftOffset = elementX + targetWidth / 2;
+    leftOffset = leftOffset - copyPopup.offsetWidth / 2;
+    copyPopup.style.top = `calc(${elementY}px - 40px)`;
+    copyPopup.style.left = `${leftOffset}px`;
+    copyPopup.classList.add("visible");
+    setTimeout(() => {
+        copyPopup.classList.remove("visible");
+    }, 100);
 }
 
 function copyIframe() {
-	querySelector = document.querySelector("code");
-	navigator.clipboard.writeText(querySelector.innerText);
-	window.getSelection().selectAllChildren(querySelector);
+    querySelector = document.querySelector("code");
+    navigator.clipboard.writeText(querySelector.innerText);
+    window.getSelection().selectAllChildren(querySelector);
 }
 
 window.copyIframe = copyIframe;
