fdf7fb9ea4ddc1322556ddf6b9a7f91d30f274fd
Pre1ude0 <larss.jakobsons@icloud.com>
2025-04-13 03:31:17 +0300

    improvements in site accessibility

--- public/js/main.js
+++ public/js/main.js
@@ -1,53 +1,51 @@
 const indicator = document.getElementById("indicator");
-
+const unknownPage = document.getElementById("unknownPage");
 const copyPopup = document.getElementById("copyPopup");
 
-function changeScreen(screenId) {
+function changeScreen() {
+    screenId = window.location.hash.substring(1);
+    unknownPage.classList.add("hidden");
+    indicator.classList.remove("hidden");
+    switch (window.location.hash) {
+        case "#connections":
+            indicator.style.bottom = "65px";
+            break;
+        case "#projects":
+            indicator.style.bottom = "15px";
+            break;
+        case "#home":
+            indicator.style.bottom = "115px";
+            break;
+        case "#hireme":
+            indicator.style.bottom = "calc(100% - 100px)";
+            indicator.classList.add("hidden");
+            break;
+        default:
+            indicator.style.opacity = "0";
+            unknownPage.classList.remove("hidden");
+            break;
+    }
     for (i = 0; i < document.getElementsByClassName("screen").length; i++) {
         if (document.getElementsByClassName("screen")[i].id !== screenId) {
             document.getElementsByClassName("screen")[i].style.opacity = "0";
             document.getElementsByClassName("screen")[i].style.visibility = "hidden";
             document.getElementsByClassName("screen")[i].style.zIndex = "0";
+        } else {
+            document.getElementById(screenId).style.opacity = "1";
+            document.getElementById(screenId).style.visibility = "visible";
+            document.getElementById(screenId).style.zIndex = "5";
         }
-        document.getElementById(screenId).style.opacity = "1";
-        document.getElementById(screenId).style.visibility = "visible";
-        document.getElementById(screenId).style.zIndex = "5";
     }
 }
 
-window.addEventListener("hashchange", function () {
-    if (window.location.hash === "#connections") {
-        indicator.style.bottom = "65px";
-        changeScreen("screen2");
-    } else if (window.location.hash === "#projects") {
-        indicator.style.bottom = "15px";
-        changeScreen("screen3");
-    } else if (window.location.hash === "#home") {
-        indicator.style.bottom = "115px";
-        changeScreen("screen1");
+window.addEventListener("hashchange", changeScreen);
+window.addEventListener("load", function () {
+    if (window.location.hash === "" || window.location.hash === "#" || window.location.hash === "#home") {
+        window.location.hash = "#home";
+    } else {
+        document.getElementsByClassName("tab-selector")[0].classList.add("not-home");
     }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    if (window.location.hash === "#connections") {
-        document
-            .getElementsByClassName("tab-selector")[0]
-            .classList.add("not-home");
-        indicator.style.bottom = "65px";
-        changeScreen("screen2");
-    } else if (window.location.hash === "#projects") {
-        document
-            .getElementsByClassName("tab-selector")[0]
-            .classList.add("not-home");
-
-        indicator.style.bottom = "15px";
-        changeScreen("screen3");
-    } else if (window.location.hash === "#home") {
-        document.getElementById("indicator").style.bottom = "115px";
-        changeScreen("screen1");
-    }
-
-    let banners = document.getElementsByClassName("banners")[0].children;
+    changeScreen();
 });
 
 function copyItem(item) {
@@ -73,3 +71,18 @@ function copyIframe() {
 
 window.copyIframe = copyIframe;
 window.copyItem = copyItem;
+
+// function showGlowingLights() {
+//     const lights = document.getElementsByClassName("glowing-light");
+//     for (let i = 0; i < lights.length; i++) {
+//         lights[i].classList.add("glow");
+//     }
+// }
+
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // tab key
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+window.addEventListener('keydown', handleFirstTab);
