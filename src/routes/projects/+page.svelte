22c1f6b7f8675754f43b6f3d1d2b8a6e3f45f080
Pre1ude0 <laura@pre1ude.dev>
2025-09-04 20:52:27 +0300

    this looks so ass let me try again

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -31,46 +31,6 @@
     ];
 </script>
 
-<div class="flex flex-row flex-wrap items-start justify-start gap-6">
-    {#each projects as project}
-        <div
-            class="overflow-hidden w-130 h-80 mb-4 bg-zinc-900 rounded-2xl border border-zinc-800"
-        >
-            <img
-                src={project.image}
-                alt={project.name}
-                class="w-full h-40 object-cover hover:object-contain mb-2 transition-all"
-            />
-            <div class="flex flex-col gap-2 p-4">
-                <div class="flex flex-row justify-between w-full">
-                    <h2 class="text-xl font-bold text-zinc-100 mb-1">
-                        {project.name}
-                    </h2>
-                    <div class="flex flex-row gap-3 text-zinc-300 text-lg">
-                        {#if project.link}
-                            <a
-                                class="outline-1 p-1 aspect-square flex items-center justify-center rounded-sm hover:bg-zinc-800 transition-colors"
-                                href={project.link}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                aria-label="Visit website"
-                                ><i class="nf nf-fa-globe"></i></a
-                            >
-                        {/if}
-                        {#if project.github}
-                            <a
-                                class="outline-1 p-1 aspect-square flex items-center justify-center rounded-sm hover:bg-zinc-800 transition-colors"
-                                href={"https://github.com/" + project.github}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                aria-label="View source on GitHub"
-                                ><i class="nf nf-fa-github"></i></a
-                            >
-                        {/if}
-                    </div>
-                </div>
-                <p class="text-zinc-300 mb-2">{project.description}</p>
-            </div>
-        </div>
-    {/each}
+<div class="w-full h-50 flex items-center justify-center text-white text-5xl">
+    This looks so ass let me try again
 </div>
