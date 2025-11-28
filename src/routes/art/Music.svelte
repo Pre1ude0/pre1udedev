6525650df00e6d7bd1083301126d3000689bc504
Pre1ude0 <laura@pre1ude.dev>
2025-11-28 23:20:46 +0100

    add new music to music page

--- src/routes/art/Music.svelte
+++ src/routes/art/Music.svelte
@@ -1,22 +1,18 @@
+<script>
+    const videoIds = ["uYyio-6wBNg", "Ty9HFFSCJWM", "_y9TjiABLrE"];
+</script>
+
 <div class="flex flex-row gap-2 flex-wrap">
-    <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/Ty9HFFSCJWM?si=ZH8AhpwFyOH9zVSj"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-    ></iframe>
-    <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/_y9TjiABLrE?si=AyxEuiO6aOmsEBWv"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-    ></iframe>
+    {#each videoIds as id}
+        <iframe
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${id}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+        ></iframe>
+    {/each}
 </div>
